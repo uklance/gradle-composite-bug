4e79fb6409a17069fec4a9ece47beca24cd390b5
Lance <uklance@gmail.com>
2020-10-31 07:18:51 +0000

    Update ConcreteTask.java

--- plugin-consumer/buildSrc/src/main/groovy/com/example/ConcreteTask.java
+++ plugin-consumer/buildSrc/src/main/groovy/com/example/ConcreteTask.java
@@ -1,6 +1,6 @@
 package com.example
 
-class ConcreteTask extends com.example.AbstractTask {
+public class ConcreteTask extends com.example.AbstractTask {
 	String getGreeting() {
 		return 'Hello World!'
 	}
